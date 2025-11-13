22afac8272b97737823317d162adc4177e2eece6
Kalle Lindqvist <kallelin@gmail.com>
2025-11-13 10:47:48 +0100

    Refactor pokemon seeder in Program

--- PokemonSimulator/Program.cs
+++ PokemonSimulator/Program.cs
@@ -9,17 +9,7 @@ internal class Program
     static void Main(string[] args)
     {
 
-        Attack flamethrower = new Attack(name: "Flamethrower", type: ElementType.Fire, basePower: 12);
-        Attack ember = new Attack(name: "Ember", type: ElementType.Fire, basePower: 8);
-        Attack waterGun = new Attack(name: "Water Gun", type: ElementType.Water, basePower: 6);
-        Attack bubble = new Attack(name: "Bubble", type: ElementType.Water, basePower: 5);
-        Attack vineWhip = new Attack(name: "Vine Whip", type: ElementType.Grass, basePower: 7);
-        Attack razorLeaf = new Attack(name: "Razor Leaf", type: ElementType.Grass, basePower: 10);
-
-        List<Pokemon> pokemons = new List<Pokemon>();
-        pokemons.Add(new Charmander(level: 1, new List<Attack>() { flamethrower, ember }));
-        pokemons.Add(new Squirtle(level: 1, new List<Attack>() { waterGun, bubble }));
-        pokemons.Add(new Bulbasaur(level: 1, new List<Attack>() { vineWhip, razorLeaf }));
+        List<Pokemon> pokemons = SeedPokemons();
 
         while (true)
         {
@@ -29,10 +19,10 @@ internal class Program
             ui.PrintEmptyLines();
             foreach (Pokemon pokemon in pokemons)
             {
+                ui.PrintEmptyLines();
                 ui.PrintLine($"Training {pokemon.Name} (Level {pokemon.Level})...");
                 try
                 {
-                    ui.PrintEmptyLines();
                     pokemon.RaiseLevel();
                     ui.PrintEmptyLines();
                     int attackIndex = AskForAttackIndex(pokemon);
@@ -52,7 +42,7 @@ internal class Program
             ui.PrintLine("Training round completed!");
             ui.Print($"Press anything to continue training, or 'q' to quit.{Environment.NewLine}");
             ConsoleKey key = Console.ReadKey(true).Key;
-            if(key == ConsoleKey.Q)
+            if (key == ConsoleKey.Q)
             {
                 ui.Print("bye");
                 break;
@@ -61,13 +51,29 @@ internal class Program
 
     }
 
+    private static List<Pokemon> SeedPokemons()
+    {
+        Attack flamethrower = new Attack(name: "Flamethrower", type: ElementType.Fire, basePower: 12);
+        Attack ember = new Attack(name: "Ember", type: ElementType.Fire, basePower: 8);
+        Attack waterGun = new Attack(name: "Water Gun", type: ElementType.Water, basePower: 6);
+        Attack bubble = new Attack(name: "Bubble", type: ElementType.Water, basePower: 5);
+        Attack vineWhip = new Attack(name: "Vine Whip", type: ElementType.Grass, basePower: 7);
+        Attack razorLeaf = new Attack(name: "Razor Leaf", type: ElementType.Grass, basePower: 10);
+
+        List<Pokemon> pokemons = new();
+        pokemons.Add(new Charmander(level: 1, new List<Attack>() { flamethrower, ember }));
+        pokemons.Add(new Squirtle(level: 1, new List<Attack>() { waterGun, bubble }));
+        pokemons.Add(new Bulbasaur(level: 1, new List<Attack>() { vineWhip, razorLeaf }));
+        return pokemons;
+    }
+
     private static int AskForAttackIndex(Pokemon pokemon)
     {
         return ui.SelectInput<int>(
             options: pokemon.Attacks
                 .Select((attack, index) => new { attack, index })
                 .ToDictionary(a => (char)('A' + a.index), a => a.index),
-            displayFunc: attackIndex => 
+            displayFunc: attackIndex =>
             {
                 Attack attack = pokemon.Attacks.ElementAt(attackIndex);
                 return $"{attack.Name} (Type: {attack.Type}, Power: {attack.BasePower})";
